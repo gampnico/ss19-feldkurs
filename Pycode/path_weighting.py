023aae0e296072973b9de31af9003abf58b8b04c
echternic <45390064+echternic@users.noreply.github.com>
2019-04-09 00:19:11 +0200

    Path weighting

---
+++ Pycode/path_weighting.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+"""Nicolas Gampierakis (2019). Scintillometer path weighting and
+effective height calculator from path position and normalised height.
+"""
+
+import math
+
+import numpy as np
+import pandas as pd
+import scipy as sp
+from scipy import integrate, special
+
+
+def bessel_second(x):
+    """Calculates the bessel function for the specific path position.
+
+    Args:
+        x (float): path position, m
+
+    Returns:
+        y (float): bessel function of path point
+    """
+    bessel_variable = 2.283 * math.pi * (x - 0.5)
+    if bessel_variable == 0:
+        y = 1
+    else:
+        y = 2 * (sp.special.jv(1, bessel_variable)) / bessel_variable
+    return y
+
+
+def pwf(path_position):
+    """Path weighting function for effective path height
+
+    Args:
+        path_position (Series): input data array
+    Returns:
+        weight (float): weight
+    """
+    weight = []
+    for i in path_position:
+        weight.append(2.163 * bessel_second(i))
+    return weight
+
+
+def effective_z(path_height, path_position):
+    """Calculates the effective path height across the entire
+    scintillometer path based on actual path height and position.
+
+    Args:
+        path_height (Series): actual path height, m
+        path_position (Series): the position along the
+            scintillometer path.
+    Returns:
+        z_eff (float): effective path height, m
+
+    """
+    # b = 1  # No height dependency
+    b = -2 / 3  # Stable
+    # b = - 4/3  # Unstable
+    ph_list = np.multiply(path_height ** b, pwf(path_position))
+    z_eff = ((sp.integrate.trapz(ph_list)) / (sp.integrate.trapz(pwf(
+        path_position)))) ** (1 / b)
+    return z_eff
+
+
+path_height_data = pd.read_csv(
+    "../MATLAB/path_height.csv", header=None,
+    names=["path_height", "norm_position"])
+
+effective_path_height = effective_z(
+    path_height_data["path_height"], path_height_data["norm_position"])
+
+mean_path_height = np.mean(path_height_data["path_height"])
+
+print("Mean path height: " + str(mean_path_height) + "m")
+print("Effective path height: " + str(effective_path_height) + "m")
